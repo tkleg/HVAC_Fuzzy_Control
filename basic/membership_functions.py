a0313bb12e0a1b1aab0983acb42a16d0be2dfe09
tkleg <thkleger@gmail.com>
2025-09-28 22:08:34 +0000

    membership functions for temp done, humidity partially done, and creation of membership function images done

--- basic/membership_functions.py
+++ basic/membership_functions.py
@@ -10,13 +10,27 @@ humidity = ctrl.Antecedent(HUM_UNIVERSE, 'humidity')      # 0 to 100%
 #delta_temp = ctrl.Antecedent(np.linspace(-100, 100, num_points), 'delta_temp')  # -100 to 100 degrees Celsius change
 #delta_humidity = ctrl.Antecedent(np.linspace(-100, 100, num_points), 'delta_humidity')  # -100 to 100% change
 
-temperature['freezing'] = 1 - fuzz.trapmf(TEMP_UNIVERSE, [-20, 10, TEMP_MAX, TEMP_MAX])
+temperature['freezing'] = 1 - fuzz.trapmf(TEMP_UNIVERSE, [-20, 0, TEMP_MAX, TEMP_MAX])
+temperature['cold'] = fuzz.trapmf(TEMP_UNIVERSE, [-20, 0, 5, 8])
+temperature['warm'] = fuzz.trapmf(TEMP_UNIVERSE, [5, 10, 20, 30])
+temperature['hot'] = fuzz.trapmf(TEMP_UNIVERSE, [20, 35, TEMP_MAX, TEMP_MAX])
 
-# Access the membership values correctly
-plt.plot(TEMP_UNIVERSE, temperature['freezing'].mf, label='Freezing')
-plt.xlabel('Temperature (°C)')
-plt.ylabel('Membership')
-plt.title('Temperature Membership Functions')
-plt.legend()
-plt.grid(True)
-plt.savefig('basic/images/universes_freezing.png', dpi=300)
+humidity['dry'] = 1 - fuzz.trapmf(HUM_UNIVERSE, [0, 20, HUM_MAX, HUM_MAX])
+humidity['comfortable'] = fuzz.trapmf(HUM_UNIVERSE, [20, 30, 50, 60])
+
+variables = {'temperature': temperature, 'humidity': humidity}
+units = {'temperature': '°C', 'humidity': '%', 'delta_temp': '°C', 'delta_humidity': '%'}
+
+for var_name, fuzzySets in variables.items():
+
+    for fuzzySetName in fuzzySets:
+        plt.plot(TEMP_UNIVERSE, fuzzySets[fuzzySetName].mf, label=fuzzySetName.title())
+
+    plt.title(var_name.title() + ' Membership Functions', fontweight='bold')
+    plt.xlabel(var_name.title() + ' (' + units[var_name] + ')')
+    plt.ylabel('Membership')
+    plt.grid(True)
+    plt.legend(title='Fuzzy Sets')
+    filename = f'basic/images/{var_name}_membership.png'
+    plt.savefig(filename, dpi=300, bbox_inches='tight')
+    plt.clf()
